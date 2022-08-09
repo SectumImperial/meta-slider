dfc2d7822fb5202f7c283101dfa5684f8c17c240
SectumImperial <sheterons@gmail.com>
2022-08-09 17:15:45 +0300

    Added validation for simpl cases

--- src/Model/Validator.ts
+++ src/Model/Validator.ts
@@ -1,10 +1,12 @@
 import { ModelInterface } from "./modelInterface";
+import initialState from "./state";
 
 class Validator {
-  min!: number;
-  max!: number;
-  value!: number;
-  step!: number;
+  private min!: number;
+  private max!: number;
+  private value!: number;
+  private step!: number;
+  private resultObject: ModelInterface;
 
   constructor(data: ModelInterface) {
     const { min, max, value, step } = data;
@@ -12,10 +14,49 @@ class Validator {
     this.max = max;
     this.value = value;
     this.step = step;
+
+    this.resultObject = initialState;
   }
 
   public validateData() {
+    this.checkRange();
+    this.checkStep();
+    this.checkValue();
 
+    return this.resultObject;
+  }
+
+  private checkRange() {
+    if (this.min === this.max) {
+      this.max = this.min + this.step;
+    }
+
+    if (this.min > this.max) {
+      [this.min, this.max] = [this.max, this.min];
+    }
+    this.resultObject.min = this.min;
+    this.resultObject.max = this.max;
+  }
+
+  private checkStep() {
+    const allRange = this.max - this.min;
+
+    if (this.step > allRange) {
+      this.step = allRange
+    }
+    this.resultObject.step = this.step;
+  }
+
+  private checkValue() {
+    if (this.value > this.max) {
+      this.value = this.max;
+    }
+
+    if (this.value < this.min) {
+      this.value = this.min;
+    }
+
+    this.resultObject.value = this.value;
   }
 }
 
