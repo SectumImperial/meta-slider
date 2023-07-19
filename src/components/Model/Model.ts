a1a9e7360e48e1800e55072894b43ef154a7df32
SectumImperial <sheterons@gmail.com>
2023-07-19 20:50:08 +0300

    fixed model

--- src/components/Model/Model.ts
+++ src/components/Model/Model.ts
@@ -2,26 +2,18 @@ import {
   ModelOptions,
   ModelValue,
   ThumbId,
-  ThumbValPercent,
   HandledMoveModel,
-  HandledMoveModelFrom,
-  HandledMoveModelTo,
-  isValueTheSamePos,
 } from '@src/components/Interfaces';
 
 class Model {
   private state: ModelOptions;
 
-  private stepPercent?: number;
-
   constructor(state: ModelOptions) {
     this.state = state;
-    this.init();
   }
 
-  public setState(state: object): void {
-    const oldState = this.state;
-    this.state = { ...oldState, ...state };
+  public setState(state: Partial<ModelOptions>): void {
+    this.state = { ...this.state, ...state };
   }
 
   public getState(): ModelOptions {
@@ -34,59 +26,26 @@ class Model {
   }
 
   public getValue(val: ModelValue): number | undefined | boolean {
-    return this.state[`${val}`];
+    return this.state[val];
   }
 
   public getPercentVal(): number {
     const { valueFrom, min, max } = this.state;
     const range: number = max - min;
-    const percent = Number(((valueFrom / range) * 100).toFixed(3));
-    return percent;
-  }
-
-  private init(): void {
-    this.stepPercent = Number((this.state.step / this.findValPercent()));
+    return Number(((valueFrom / range) * 100).toFixed(3));
   }
 
   private findSuitablePercent(percentMove: number, thumb: ThumbId): HandledMoveModel {
-    const {
-      step,
-      max,
-      min,
-    } = this.state;
-
-    if (this.stepPercent === undefined) this.stepPercent = 1;
-    const nearestPrevCountStep = Math.floor(percentMove / this.stepPercent);
-    const nearestNextCountStep = Math.ceil(percentMove / this.stepPercent);
-
-    const prevStep = min + (step * nearestPrevCountStep);
-    const nextStep = min + (step * nearestNextCountStep);
-
-    const prevPercent = this.stepPercent * nearestPrevCountStep;
-    const nextPercent = this.stepPercent * nearestNextCountStep;
-
-    const halfStep = Number((this.stepPercent / 2).toFixed(3));
-    const halfMove = Number((percentMove % this.stepPercent).toFixed(3));
+    const { step, min } = this.state;
+    const nearestStep = Math.round(percentMove / (100 / this.getStepsCount()));
+    const percent = (100 / this.getStepsCount()) * nearestStep;
 
     let value: number;
-    let percent: number | undefined;
-
-    if (halfMove < halfStep) {
-      value = prevStep;
-      percent = prevPercent;
+    if (percent === 100) {
+      value = this.state.max;
     } else {
-      value = nextStep;
-      percent = nextPercent;
+      value = min + step * nearestStep;
     }
-
-    const allSteps = Math.ceil((max - min) / step);
-    const beforeEndPercent = this.stepPercent * (allSteps - 1);
-
-    if (percentMove === 100 || 100 - ((100 - beforeEndPercent) / 2) < percentMove) {
-      value = max;
-      percent = 100;
-    }
-    if (percent === undefined) percent = this.stepPercent * nearestPrevCountStep;
 
     return {
       value,
@@ -95,116 +54,25 @@ class Model {
     };
   }
 
+  private getStepsCount(): number {
+    return Math.ceil((this.state.max - this.state.min) / this.state.step);
+  }
+
   private handleMove(values: HandledMoveModel): void {
-    const {
-      value,
-      thumb,
-      percent,
-    } = values;
-
-    if (thumb === 'valueFrom') {
-      this.handleMoveFrom({
-        value,
-        thumb,
-        percent,
-      });
-    }
-
-    if (thumb === 'valueTo') {
-      this.handleMoveTo({
-        value,
-        thumb,
-        percent,
-      });
-    }
-  }
-
-  private handleMoveFrom(values: HandledMoveModelFrom): void {
-    const {
-      value,
-      thumb,
-      percent,
-    } = values;
-
-    const {
-      valueTo,
-      thumbPercentTo,
-    } = this.state;
-
-    if (this.isValueTheSamePos({
-      value,
-      valueAnotherThumb: valueTo,
-      thumbPercent: thumbPercentTo,
-      thumb,
-      idVal: 'valueFrom',
-    }) && valueTo !== undefined && thumbPercentTo !== undefined) {
-      this.updateMoved(valueTo, thumbPercentTo, thumb);
-    } else if (value !== undefined) this.updateMoved(value, percent, thumb);
-  }
-
-  private handleMoveTo(values: HandledMoveModelTo): void {
-    const {
-      value,
-      thumb,
-      percent,
-    } = values;
-
-    const {
-      valueFrom,
-      thumbPercentFrom,
-    } = this.state;
-
-    if (this.isValueTheSamePos({
-      value,
-      valueAnotherThumb: valueFrom,
-      thumbPercent: thumbPercentFrom,
-      thumb,
-      idVal: 'valueTo',
-    }) && valueFrom !== undefined) {
-      this.updateMoved(valueFrom, thumbPercentFrom, thumb);
-    } else if (value !== undefined) {
+    const { value, thumb, percent } = values;
+    if (value !== undefined) {
       this.updateMoved(value, percent, thumb);
     }
   }
 
-  private isValueTheSamePos(values: isValueTheSamePos) {
-    const {
-      value,
-      valueAnotherThumb,
-      thumbPercent,
-      thumb,
-      idVal,
-    } = values;
-
-    const { isRange } = this.state;
-
-    const checkVal = isRange && thumbPercent !== undefined && thumb === idVal;
-    let compareVal = false;
-    if (value !== undefined && valueAnotherThumb !== undefined) {
-      compareVal = idVal === 'valueFrom' ? value > valueAnotherThumb : value < valueAnotherThumb;
-    }
-
-    return checkVal && compareVal;
-  }
-
-  private findValPercent(): number {
-    const range = this.findRange();
-    const percent = range / 100;
-    return percent;
-  }
-
-  private findRange(): number {
-    const range = this.state.max - this.state.min;
-    return range;
-  }
-
   private updateMoved(val: number, percent: number, thumb: ThumbId): void {
-    if (Number.isNaN(val) || percent === undefined) throw new Error('Something wrong with setting new values');
-    const thumbPercent = ThumbValPercent[thumb];
+    if (Number.isNaN(val) || Number.isNaN(percent)) {
+      throw new Error('Invalid value or percent provided while updating the model.');
+    }
 
     this.setState({
       [thumb]: val,
-      [thumbPercent]: percent,
+      thumbPercentTo: percent,
     });
   }
 }
